40b51f3e032573b12fb06cd9f0ae799c616e2004
Johann Diedrick <jdiedrick@gmail.com>
2015-04-27 07:18:56 -0400

    move main view when keyboard is shown, simple implementation

--- Jouzu/ViewController.swift
+++ Jouzu/ViewController.swift
@@ -80,8 +80,24 @@ class ViewController: UIViewController, UITextFieldDelegate, FlashCardGameModelD
         updateUI();
         return false;
     }
+    func textFieldDidBeginEditing(textField: UITextField) {
+        animateViewMoving(true, moveValue: 100)
+    }
+
     func textFieldDidEndEditing(textField: UITextField) {
         println("\(textField.text)")
+        animateViewMoving(false, moveValue: 100)
+    }
+    
+    
+    func animateViewMoving (up:Bool, moveValue :CGFloat){
+        var movementDuration:NSTimeInterval = 0.3
+        var movement:CGFloat = ( up ? -moveValue : moveValue)
+        UIView.beginAnimations( "animateView", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration )
+        self.view.frame = CGRectOffset(self.view.frame, 0,  movement)
+        UIView.commitAnimations()
     }
     
     func displayAlert(alert: UIAlertController) {
